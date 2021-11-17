84d84f9c8c746d9979f39d20178bff2654987065
Ronald Benítez <ronald362001@gmail.com>
2021-11-17 09:30:15 -0600

    First commit

---
+++ database/migrations/2021_11_17_145634_create_peliculas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeliculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('synopsis');
+            $table->string('img');
+            $table->integer('likes');
+            $table->integer('reserved');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+}
