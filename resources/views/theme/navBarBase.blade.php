d2ae3447ed3f8bd21182a4c2f1b9a8f6ebdc1307
Ronald Benítez <ronald362001@gmail.com>
2021-11-20 21:45:43 -0600

    Navbar mejoradas

--- resources/views/theme/navBarBase.blade.php
+++ resources/views/theme/navBarBase.blade.php
@@ -1,41 +1,3 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-        {{-- icono --}}
-        <a class="navbar-brand" href="{{ route('Pelicula.index') }}">
-            <img src="https://img.icons8.com/doodle/48/000000/minecraft-grass-cube.png" alt="" width="30" height="24">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown"
-            aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-            {{-- Valorez Izquierda --}}
-            <ul class="navbar-nav">
-                <li class="nav-item me-4">
-                    <a class="nav-link active" aria-current="page" href="{{ route('Pelicula.index') }}">Inicio</a>
-                </li>
-                {{-- busqueda --}}
-                <form class="d-flex" action="{{ route('Filtro.search') }}" method="POST">
-                    @csrf
-                    <input class="form-control me-3" type="search" name="search" placeholder="Nombre de pelicula."
-                        aria-label="">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-        </div>
-        {{-- Valores Derecha --}}
-        <div class="mx-2">
-            <a type="button" class="btn btn-outline-info" href="{{ route('login.loguear') }}">Iniciar sesión</a>
-            <a type="button" class="btn btn-outline-warning ms-1" href="{{ route('Usuario.create') }}">Registrarse</a>
-        </div>
-
-    </div>
-
-    </div>
-
-    </div>
-</nav>
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('Pelicula.index') }}">
@@ -51,16 +13,44 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{ route('Pelicula.index') }}">Inicio</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-info" aria-current="page" href="{{ route('login.loguear') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                            <path fill-rule="evenodd"
+                                d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                        </svg>
+                        Iniciar sesión
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-warning" aria-current="page" href="{{ route('Usuario.create') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-person-plus" viewBox="0 0 16 16">
+                            <path
+                                d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                            <path fill-rule="evenodd"
+                                d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                        </svg>
+                        Registrarse
+                    </a>
+                </li>
+
             </ul>
             <form class="d-flex" action="{{ route('Filtro.search') }}" method="POST">
                 @csrf
-                <input class="form-control me-3" type="search" name="search" placeholder="Nombre de pelicula."
+                <input class="form-control me-1" type=" search" name="search" placeholder="Nombre de pelicula."
                     aria-label="">
-                <button class="btn btn-outline-info" type="submit">Buscar</button>
+                <button class="btn btn-info" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-search" viewBox="0 0 16 16">
+                        <path
+                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                </button>
             </form>
-            <a type="button" class="btn btn-info mx-2" href="{{ route('login.loguear') }}">Iniciar
-                sesión</a>
-            <a type="button" class="btn btn-warning mx-2" href="{{ route('Usuario.create') }}">Registrarse</a>
 
         </div>
 
